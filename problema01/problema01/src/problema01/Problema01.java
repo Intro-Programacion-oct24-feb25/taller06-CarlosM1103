1ab1cacbcdba4c353e0ef412edcd861a9a9a05c9
CarlosM1103 <carlosmedinal2006@gmail.com>
2024-11-14 11:57:10 -0500

    hjsdhj

---
+++ problema01/problema01/src/problema01/Problema01.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema01;
+import java.util.Scanner;
+/**
+ *
+ * @author utpl
+ */
+public class Problema01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        
+        double num1;
+        double num2;
+        double resultado;
+        
+        System.out.println("Ingrese el primer número");
+        num1 = leer.nextDouble();
+        System.out.println("Ingrese el segundo número");
+        num2 = leer.nextDouble();
+    }
+    
+}
